b0f942fdd18cf941a028f9d2c729fd292f2bd7f4
Franco Voisard <franm.voisard@etermax.com>
2022-12-16 00:33:12 -0300

    Cell cannot move to negative positions

--- Assets/Scripts/Runtime/Domain/Cell.cs
+++ Assets/Scripts/Runtime/Domain/Cell.cs
@@ -1,26 +1,37 @@
+using System;
 using UnityEngine;
 
 namespace Shoelace.Bejeweld
 {
     public class Cell
     {
-        public Vector2 Position { get; private set; }
+        public Vector2Int GridPosition { get; private set; }
         
-        public Cell(Vector2 cellOneInitialPosition)
+        public Cell(Vector2Int cellOneInitialGridPosition)
         {
-            Position = cellOneInitialPosition;
+            GridPosition = cellOneInitialGridPosition;
         }
 
-        public void Swap(Cell cellTwo)
+        public void Swap(Cell target)
         {
-            var auxPosition = Position;
-            Position = cellTwo.Position;
-            cellTwo.SetPosition(auxPosition);
+            if (IsValidPosition(target) == false)
+            {
+                throw new ArgumentException($"Cannot swap to a position with negative values. ({target.GridPosition})");
+            }
+            
+            var auxPosition = GridPosition;
+            SetPosition(target.GridPosition);
+            target.SetPosition(auxPosition);
         }
 
-        private void SetPosition(Vector2 position)
+        private static bool IsValidPosition(Cell target)
         {
-            Position = position;
+            return target.GridPosition.x > 0 && target.GridPosition.y > 0;
+        }
+
+        private void SetPosition(Vector2Int position)
+        {
+            GridPosition = position;
         }
     }
 }
--- Assets/Scripts/Tests/Editor/CellShould.cs
+++ Assets/Scripts/Tests/Editor/CellShould.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Shoelace.Bejeweld;
 using UnityEngine;
@@ -7,11 +8,11 @@ namespace Tests.Editor
     public class CellShould
     {
         [Test]
-        public void Update_Position_When_Swap_Positions_With_Another_Cell()
+        public void Update_Position_When_Swap_Position_With_Another_Cell()
         {
             //Given
-            var cellOneInitialPosition = new Vector2(1, 1);
-            var cellTwoInitialPosition = new Vector2(2, 1);
+            var cellOneInitialPosition = new Vector2Int(1, 1);
+            var cellTwoInitialPosition = new Vector2Int(2, 1);
             
             var cellOne = new Cell(cellOneInitialPosition);
             var cellTwo = new Cell(cellTwoInitialPosition);
@@ -20,8 +21,24 @@ namespace Tests.Editor
             cellOne.Swap(cellTwo);
             
             //Then
-            Assert.AreEqual(cellOne.Position,cellTwoInitialPosition);
-            Assert.AreEqual(cellTwo.Position,cellOneInitialPosition);
+            Assert.AreEqual(cellOne.GridPosition,cellTwoInitialPosition);
+            Assert.AreEqual(cellTwo.GridPosition,cellOneInitialPosition);
+        }
+        
+        [Test]
+        public void Throw_ArgumentException_When_Attempt_To_Swap_To_A_Negative_Position()
+        {
+            //Given
+            var cellInitialPosition = new Vector2Int(1, 1);
+            var cell = new Cell(cellInitialPosition);
+            var cellTwo = new Cell(new Vector2Int(-1,1));
+            
+            //Then
+            Assert.Throws<ArgumentException>(() =>
+            {
+                //When
+                cell.Swap(cellTwo);
+            });
         }
     }
 }
