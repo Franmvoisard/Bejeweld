5de145815718def7edec59dc48d85e96ad9de2f4
Franco Voisard <franm.voisard@gmail.com>
2022-12-22 03:56:55 -0300

    Added visual feedback when match

--- Assets/Scripts/Game/Delivery/Components/LevelHUDView.cs
+++ Assets/Scripts/Game/Delivery/Components/LevelHUDView.cs
@@ -1,9 +1,8 @@
 using System;
-using Codice.Client.BaseCommands;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using Object = UnityEngine.Object;
 
 namespace Shoelace.Bejeweld.Components
 {
@@ -12,17 +11,49 @@ namespace Shoelace.Bejeweld.Components
         [SerializeField] private TMP_Text scoreText;
         [SerializeField] private TMP_Text levelText;
         [SerializeField] private Slider progressBar;
-        
+        [SerializeField] private Vector3 maxScale;
+        [SerializeField] private float upTextDuration;
+        [SerializeField] private float downTextDuration;
+        private Coroutine _scaleUpCoroutine;
+
         public void UpdateProgress(int score)
         {
             scoreText.text = score.ToString();
             progressBar.value = score;
+            ScaleTextAnimation();
         }
 
         public void SetLevel(Level level)
         {
             levelText.text = "LEVEL " + level.levelId;
             progressBar.maxValue = level.goal;
+        }
+
+        private void ScaleTextAnimation()
+        {
+            if (_scaleUpCoroutine != null) StopCoroutine(_scaleUpCoroutine);
+            _scaleUpCoroutine = StartCoroutine(ScaleUpText(upTextDuration, downTextDuration));
+        }
+
+        private IEnumerator ScaleUpText(float upDuration, float downDuration)
+        {
+            float time = 0;
+            var initialScale = scoreText.transform.localScale;
+            while (time < upDuration)
+            {
+                time += Time.deltaTime;
+                scoreText.transform.localScale = Vector3.Lerp(initialScale, maxScale, time / upDuration);
+                yield return null;
+            }
+
+            time = 0;
+            
+            while (time < downDuration)
+            {
+                time += Time.deltaTime;
+                scoreText.transform.localScale = Vector3.Lerp(maxScale, Vector3.one, time / downDuration);
+                yield return null;
+            }
         }
     }
 
